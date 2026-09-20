2460b90ae275e9763cb4abdfcb37928b3b533e53
demo <abhishekshinde5805@gmail.com>
2026-09-20 21:18:29 +0530

    added new filrd

--- celsiusToFahrenheit.py
+++ celsiusToFahrenheit.py
@@ -1,3 +1,5 @@
+# write a python program to convert temperature in Celsius to Fahrenheit.
+
 cel_temp=int(input("Enter temperature in Celsius: "))
 fer_temp=(cel_temp*1.8)+32
 print("Temperature in Fahrenheit:", fer_temp)
