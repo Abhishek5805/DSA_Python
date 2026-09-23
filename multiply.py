22093a59cb07671b1bb369bb34c0165dbd23dbec
demo <abhishekshinde5805@gmail.com>
2026-09-23 18:25:46 +0530

    adding nnew files

---
+++ multiply.py
@@ -0,0 +1,7 @@
+import math
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+multiply=math.prod([a,b])  #math.prod() is used to multiply the numbers in the list.
+
+print("Multiplication of two numbers is:", multiply)
